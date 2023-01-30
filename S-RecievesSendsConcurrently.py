e6dfa7807464330cd4c7c8a2a8fcdc3b23b4b161
imApoorva36 <90238207+imApoorva36@users.noreply.github.com>
2023-01-30 13:22:17 +0530

    Server code for sending and receiving concurrently

---
+++ S-RecievesSendsConcurrently.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+import logging
+
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.DEBUG,
+)
+
+async def send_messages(websocket):
+    while True:
+        await websocket.send("Hello client!")
+        await asyncio.sleep(1)
+
+
+async def receive_messages(websocket):
+    numbering = 0
+    while True:
+        message = await websocket.recv()
+        print(f"{numbering}. Received message: {message}")
+        numbering += 1
+
+
+async def server(websocket, path):
+    # Create tasks to send and receive messages concurrently
+    send_task = asyncio.create_task(send_messages(websocket))
+    receive_task = asyncio.create_task(receive_messages(websocket))
+    await asyncio.gather(send_task, receive_task)
+
+
+async def main():
+    # Start the server
+    async with websockets.serve(server, "localhost", 8000):
+        await asyncio.Future()
+
+asyncio.run(main())
